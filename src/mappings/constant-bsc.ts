56b2e00a2f562264619a1f0b7d442fca9f06d70b
牛涛涛 <15700059289@163.com>
2021-02-04 15:41:46 +0800

    constans config

---
+++ src/mappings/constant-bsc.ts
@@ -0,0 +1,35 @@
+//poo type
+export const TYPE_DVM_POOL = "DVM";
+export const TYPE_DPP_POOL = "DPP";
+export const TYPE_CLASSICAL_POOL = "CLASSICAL";
+export const SOURCE_SMART_ROUTE = "SMART_ROUTE";
+export const SOURCE_POOL_SWAP = "DODO_POOL";
+export const DODOV2Proxy01="0xb57dd5c265dbb13ca014f2332069e90cd0e22e65";
+
+export const DODOZooID = "dodoex-v2-bsc";
+export const SMART_ROUTE_ADDRESSES: string[]= ["0xb57dd5c265dbb13ca014f2332069e90cd0e22e65"];
+export const DVM_FACTORY_ADDRESS = "0xf50bdc9e90b7a1c138cb7935071b85c417c4cb8e";
+export const DPP_FACTORY_ADDRESS = "0x7737fd30535c69545deeea54ab8dd590ccaebd3c";
+export const CLASSIC_FACTORY_ADDRESS = "0xca459456a45e300aa7ef447dbb60f87cccb42828";//dodo zoo
+
+export const DVM_FACTORY_DEPLOY_BLOCK = 4208093;
+export const DPP_FACTORY_DEPLOY_BLOCK = 4208108;
+
+
+export const ADDRESS_ZERO = '0x0000000000000000000000000000000000000000'
+export const ETH_ADDRESS = '0xeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeee';
+
+export const WETH_ADDRESS = '0xc02aaa39b223fe8d0a0e5c4f27ead9083c756cc2';
+export const USDT_ADDRESS = '0xdac17f958d2ee523a2206206994597c13d831ec7';
+export const USDC_ADDRESS = '0xa0b86991c6218b36c1d19d4a2e9eb0ce3606eb48';
+
+export const WETH_USDC_PAIR = '0x75c23271661d9d143dcb617222bc4bec783eff34'; // created 10616399
+export const USDT_USDC_PAIR = '0xc9f93163c99695c6526b799ebca2207fdf7d61ad'; // created 11106395
+
+export const WETH_USDC_BLOCK = 10616399;
+export const USDT_USDC_BLOCK = 11106395;
+
+export const TRADING_INCENTIVE_ADDRESS = "0x80930cb1849f7d42531506ff45e66724338a821b";
+export  const DODO_MINE_ADDRESS = "0xaed7384f03844af886b830862ff0a7afce0a632c"
+
+
---
+++ src/mappings/constant-kovan.ts
@@ -0,0 +1,29 @@
+//poo type
+export const TYPE_DVM_POOL = "DVM";
+export const TYPE_DPP_POOL = "DPP";
+export const TYPE_CLASSICAL_POOL = "CLASSICAL";
+export const SOURCE_SMART_ROUTE = "SMART_ROUTE";
+export const SOURCE_POOL_SWAP = "DODO_POOL";
+export const DODOZooID = "dodoex-v2-kovan";
+export const SMART_ROUTE_ADDRESSES: string[]= ["0x3457a15b9ab57fc754789ee83e4bd2bd8f4f50c8"];
+export const DVM_FACTORY_ADDRESS = "0xE842d8c9A54B23C4D0cf208daCA3882c0c311353";
+export const DPP_FACTORY_ADDRESS = "0x6dab26dfe83e484dcc5126f812e3e6aa8e7eef4d";
+export const CLASSIC_FACTORY_ADDRESS = "0x92230e929a2226b29ed3441ae5524886347c60c8";
+
+export const DVM_FACTORY_DEPLOY_BLOCK = 23109610;
+export const DPP_FACTORY_DEPLOY_BLOCK = 23158541;
+
+export const ADDRESS_ZERO = '0x0000000000000000000000000000000000000000'
+export const ETH_ADDRESS = '0xeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeee';
+
+export const WETH_ADDRESS = '0x5eca15b12d959dfcf9c71c59f8b467eb8c6efd0b';
+export const USDT_ADDRESS = '0x9d48172b0189de70f8788de10196a1fb622b8e6b';
+export const USDC_ADDRESS = '0x69c8a7fc6e05d7aa36114b3e35f62deca8e11f6e';
+
+export const WETH_USDC_PAIR = '0x2b517ba87eae60de363b7295b08167ba7ee25143'; // created 10008355
+export const USDT_USDC_PAIR = '0xa478c2975ab1ea89e8196811f51a7b7ade33eb11'; // created block 10042267
+export const WETH_USDC_BLOCK = 23016939;
+export const USDT_USDC_BLOCK = 23016946;
+export const TRADING_INCENTIVE_ADDRESS = "0x1f69E3CEAbDc464Ab11bceB15726530CD8AC535E";
+export  const DODO_MINE_ADDRESS = "0xaed7384f03844af886b830862ff0a7afce0a632c";
+
