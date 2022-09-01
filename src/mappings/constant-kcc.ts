56f9f7102ebbc73386b4ceb0f11860a42b34b1ce
牛涛涛 <15700059289@163.com>
2022-09-01 13:40:24 +0800

    ADD:kcc support

--- src/mappings/constant-kcc.ts
+++ src/mappings/constant-kcc.ts
@@ -6,16 +6,16 @@ export const TYPE_DSP_POOL = "DSP";
 export const TYPE_CLASSICAL_POOL = "CLASSICAL";
 export const SOURCE_SMART_ROUTE = "SMART_ROUTE";
 export const SOURCE_POOL_SWAP = "DODO_POOL";
-export const DODOZooID = "dodoex-v2-rinkeby";
-export const CHAIN_BASE_COIN_SYMBOL = "ETH";
-export const CHAIN_BASE_COIN_NAME = "ethereum";
+export const DODOZooID = "dodoex-v2-kcc";
+export const CHAIN_BASE_COIN_SYMBOL = "KCS";
+export const CHAIN_BASE_COIN_NAME = "kcs";
 export const DIP3_TIMESTAMP = 1624442400;
 
-export const SMART_ROUTE_ADDRESSES: string[]= ["0xba001e96af87bf9d8d0bda667067a9921fe6d294"];
-export const DVM_FACTORY_ADDRESS = "0x17dddefa0c82e0c850a4adac2afe3f1fe977a242";
-export const DPP_FACTORY_ADDRESS = "0x510b49803e356c750f3a93ba5508c0ffd9f71bdd";
+export const SMART_ROUTE_ADDRESSES: string[]= ["0xf0512872fec0173d1d99c2dd8cdcb770054b675b"];
+export const DVM_FACTORY_ADDRESS = "0x31ac053c31a77055b2ae2d3899091c0a9c19ce3a";
+export const DPP_FACTORY_ADDRESS = "0x4632e6ebd4a01ebf54739a9d71a62cedb29e9183";
 export const CLASSIC_FACTORY_ADDRESS = "0x168442fec1e1e782c8770185dbb8328b91dc45c0";
-export const CROWDPOOLING_FACTORY_V2: string[]=["0x79e15fc9b4154d90424b588533ab277d30d573f4"];
+export const CROWDPOOLING_FACTORY_V2: string[]=["0xae1e7e4c5743321d2a8ceed179264998a56cf1d9"];
 
 export const DVM_FACTORY_DEPLOY_BLOCK = 8732843;
 export const DPP_FACTORY_DEPLOY_BLOCK = 8732843;
@@ -34,10 +34,10 @@ export const TRANSACTION_TYPE_CP_CLAIM = "CP_CLAIM"
 /**
  * usd pricing
  */
-export const WRAPPED_BASE_COIN = '0xb26c0d8be2960c70641a95a9195be1f59ac83ac0';
+export const WRAPPED_BASE_COIN = '0x4446fc4eb47f2f6586f9faab68b3498f86c07521';
 //pricing supported stable coins
-export const STABLE_ONE_ADDRESS = '0x0adcbae18580120667f7ff6c6451a426b13c67b7';//usdt
-export const STABLE_TWO_ADDRESS = '0xab0733588776b8881f7712f6abca98f510e6b63d';//usdc
+export const STABLE_ONE_ADDRESS = '0x0039f574ee5cc39bdd162e9a88e3eb1f111baf48';//usdt
+export const STABLE_TWO_ADDRESS = '0x980a5afef3d17ad98635f6c5aebcbaeded3c3430';//usdc
 //stable coins pairs
 export const STABLE_COIN_PAIR_ONE = '0xe8c1b7e0505c97c59ff2f12ebf91a8eaa59d4c73';//[USDT_USDC_PAIR]
 //base currency pair
