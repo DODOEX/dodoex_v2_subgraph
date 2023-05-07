94704dc30dcf8a9c3645bd2dfbcd9ebbfec7f4ec
pengyongjun <pengyongjun@vip.qq.com>
2023-05-07 22:41:10 +0800

    fix: deploy basegor error

--- src/mappings/mine/mineProxy.ts
+++ src/mappings/mine/mineProxy.ts
@@ -1,90 +1,92 @@
-import {MinePool, RewardDetail} from "../../types/mine/schema"
-import {CreateMineV3} from "../../types/mine/DODOMineV3Proxy/DODOMineV3Proxy"
-import {CreateMineV3 as V2CreateMineV3} from "../../types/mine/DODOMineV3Proxy/DODOMineV3ProxyV2"
-import {ERC20MineV3 as ERC20MineV3Template} from "../../types/mine/templates"
+import { MinePool, RewardDetail } from "../../types/mine/schema";
+import { CreateMineV3 } from "../../types/mine/DODOMineV3Proxy/DODOMineV3Proxy";
+import { CreateMineV3 as V2CreateMineV3 } from "../../types/mine/DODOMineV3ProxyV2/DODOMineV3ProxyV2";
+import { ERC20MineV3 as ERC20MineV3Template } from "../../types/mine/templates";
 
-import {getRewardNum, rewardTokenInfos, getToken} from "./helper"
-import {BigInt, log} from "@graphprotocol/graph-ts";
+import { getRewardNum, rewardTokenInfos, getToken } from "./helper";
+import { BigInt, log } from "@graphprotocol/graph-ts";
 
 export function handleCreateMineV3(event: CreateMineV3): void {
-    let minePool = MinePool.load(event.params.mineV3.toHexString());
+  let minePool = MinePool.load(event.params.mineV3.toHexString());
 
-    if (minePool == null) {
-        minePool = new MinePool(event.params.mineV3.toHexString());
+  if (minePool == null) {
+    minePool = new MinePool(event.params.mineV3.toHexString());
+  }
+  minePool.creator = event.params.account;
+  minePool.pool = event.params.mineV3;
+  minePool.timestamp = event.block.timestamp;
+  minePool.stakeToken = getToken(event.params.mineV3);
+
+  let rewardTokensNum = getRewardNum(event.params.mineV3);
+
+  for (let i = 0; i < rewardTokensNum.toI32(); i++) {
+    let rewardData = rewardTokenInfos(event.params.mineV3, BigInt.fromI32(i));
+    let detailID = event.params.mineV3
+      .toHexString()
+      .concat("-")
+      .concat(rewardData.value0.toHexString());
+    let rewardDetail = RewardDetail.load(detailID);
+
+    if (rewardDetail == null) {
+      rewardDetail = new RewardDetail(detailID);
     }
-    minePool.creator = event.params.account;
-    minePool.pool = event.params.mineV3;
-    minePool.timestamp = event.block.timestamp;
-    minePool.stakeToken = getToken(event.params.mineV3);
+    rewardDetail.minePool = minePool.id;
+    rewardDetail.token = rewardData.value0;
+    rewardDetail.startBlock = rewardData.value1;
+    rewardDetail.endBlock = rewardData.value2;
+    rewardDetail.rewardPerBlock = rewardData.value4;
+    rewardDetail.updatedAt = event.block.timestamp;
+    rewardDetail.save();
+  }
 
-    let rewardTokensNum = getRewardNum(event.params.mineV3);
+  minePool.updatedAt = event.block.timestamp;
+  minePool.save();
 
-    for (let i = 0; i < rewardTokensNum.toI32(); i++) {
-        let rewardData = rewardTokenInfos(event.params.mineV3, BigInt.fromI32(i));
-        let detailID = event.params.mineV3.toHexString().concat("-").concat(rewardData.value0.toHexString());
-        let rewardDetail = RewardDetail.load(detailID);
-
-        if (rewardDetail == null) {
-            rewardDetail = new RewardDetail(detailID);
-        }
-        rewardDetail.minePool = minePool.id;
-        rewardDetail.token = rewardData.value0;
-        rewardDetail.startBlock = rewardData.value1;
-        rewardDetail.endBlock = rewardData.value2;
-        rewardDetail.rewardPerBlock = rewardData.value4;
-        rewardDetail.updatedAt = event.block.timestamp;
-        rewardDetail.save();
-    }
-
-    minePool.updatedAt = event.block.timestamp;
-    minePool.save();
-
-    //will get "fatalError":{"message":"type mismatch with parameters: expected 1 types, found 0"
-    log.debug("mineV3 address: {}", [event.params.mineV3.toHexString()]);
-    log.info("mineV3 address: {}", [event.params.mineV3.toHexString()]);
-    if (event.params.mineV3 != null) {
-        ERC20MineV3Template.create(event.params.mineV3);
-    }
+  //will get "fatalError":{"message":"type mismatch with parameters: expected 1 types, found 0"
+  log.debug("mineV3 address: {}", [event.params.mineV3.toHexString()]);
+  log.info("mineV3 address: {}", [event.params.mineV3.toHexString()]);
+  ERC20MineV3Template.create(event.params.mineV3);
 }
 
 export function handleV2CreateMineV3(event: V2CreateMineV3): void {
-    let minePool = MinePool.load(event.params.mineV3.toHexString());
+  let minePool = MinePool.load(event.params.mineV3.toHexString());
 
-    if (minePool == null) {
-        minePool = new MinePool(event.params.mineV3.toHexString());
+  if (minePool == null) {
+    minePool = new MinePool(event.params.mineV3.toHexString());
+  }
+  minePool.creator = event.params.account;
+  minePool.pool = event.params.mineV3;
+  minePool.timestamp = event.block.timestamp;
+  minePool.platform = event.params.platform;
+  minePool.stakeToken = getToken(event.params.mineV3);
+
+  let rewardTokensNum = getRewardNum(event.params.mineV3);
+
+  for (let i = 0; i < rewardTokensNum.toI32(); i++) {
+    let rewardData = rewardTokenInfos(event.params.mineV3, BigInt.fromI32(i));
+    let detailID = event.params.mineV3
+      .toHexString()
+      .concat("-")
+      .concat(rewardData.value0.toHexString());
+    let rewardDetail = RewardDetail.load(detailID);
+
+    if (rewardDetail == null) {
+      rewardDetail = new RewardDetail(detailID);
     }
-    minePool.creator = event.params.account;
-    minePool.pool = event.params.mineV3;
-    minePool.timestamp = event.block.timestamp;
-    minePool.platform = event.params.platform;
-    minePool.stakeToken = getToken(event.params.mineV3);
+    rewardDetail.minePool = minePool.id;
+    rewardDetail.token = rewardData.value0;
+    rewardDetail.startBlock = rewardData.value1;
+    rewardDetail.endBlock = rewardData.value2;
+    rewardDetail.rewardPerBlock = rewardData.value4;
+    rewardDetail.updatedAt = event.block.timestamp;
+    rewardDetail.save();
+  }
 
-    let rewardTokensNum = getRewardNum(event.params.mineV3);
+  minePool.updatedAt = event.block.timestamp;
+  minePool.save();
 
-    for (let i = 0; i < rewardTokensNum.toI32(); i++) {
-        let rewardData = rewardTokenInfos(event.params.mineV3, BigInt.fromI32(i));
-        let detailID = event.params.mineV3.toHexString().concat("-").concat(rewardData.value0.toHexString());
-        let rewardDetail = RewardDetail.load(detailID);
-
-        if (rewardDetail == null) {
-            rewardDetail = new RewardDetail(detailID);
-        }
-        rewardDetail.minePool = minePool.id;
-        rewardDetail.token = rewardData.value0;
-        rewardDetail.startBlock = rewardData.value1;
-        rewardDetail.endBlock = rewardData.value2;
-        rewardDetail.rewardPerBlock = rewardData.value4;
-        rewardDetail.updatedAt = event.block.timestamp;
-        rewardDetail.save();
-    }
-
-    minePool.updatedAt = event.block.timestamp;
-    minePool.save();
-
-    //will get "fatalError":{"message":"type mismatch with parameters: expected 1 types, found 0"
-    log.debug("mineV3 address: {}", [event.params.mineV3.toHexString()]);
-    log.info("mineV3 address: {}", [event.params.mineV3.toHexString()]);
-    if (event.params.mineV3 != null) {
-        ERC20MineV3Template.create(event.params.mineV3);
-    }
+  //will get "fatalError":{"message":"type mismatch with parameters: expected 1 types, found 0"
+  log.debug("mineV3 address: {}", [event.params.mineV3.toHexString()]);
+  log.info("mineV3 address: {}", [event.params.mineV3.toHexString()]);
+  ERC20MineV3Template.create(event.params.mineV3);
 }
--- src/mappings/token/helpers.ts
+++ src/mappings/token/helpers.ts
@@ -1,267 +1,302 @@
 /* eslint-disable prefer-const */
-import {log, BigInt, BigDecimal, Address, ethereum} from '@graphprotocol/graph-ts'
-import {ERC20} from "../../types/token/ERC20Factory/ERC20"
-import {ERC20NameBytes} from "../../types/token/ERC20Factory/ERC20NameBytes"
-import {ERC20SymbolBytes} from "../../types/token/ERC20Factory/ERC20SymbolBytes"
-import {User, DodoToken} from "../../types/token/schema"
+import {
+  log,
+  BigInt,
+  BigDecimal,
+  Address,
+  ethereum,
+} from "@graphprotocol/graph-ts";
+import { ERC20 } from "../../types/token/ERC20Factory/ERC20";
+import { ERC20NameBytes } from "../../types/token/ERC20Factory/ERC20NameBytes";
+import { ERC20SymbolBytes } from "../../types/token/ERC20Factory/ERC20SymbolBytes";
+import { User, DodoToken } from "../../types/token/schema";
 
 import {
-    BASE_COIN,
-    CHAIN_BASE_COIN_SYMBOL,
-    CHAIN_BASE_COIN_NAME
-} from "../constant"
+  BASE_COIN,
+  CHAIN_BASE_COIN_SYMBOL,
+  CHAIN_BASE_COIN_NAME,
+} from "../constant";
 
-export let ZERO_BI = BigInt.fromI32(0)
-export let ONE_BI = BigInt.fromI32(1)
-export let ZERO_BD = BigDecimal.fromString('0')
-export let ONE_BD = BigDecimal.fromString('1')
-export let BI_18 = BigInt.fromI32(18)
+export let ZERO_BI = BigInt.fromI32(0);
+export let ONE_BI = BigInt.fromI32(1);
+export let ZERO_BD = BigDecimal.fromString("0");
+export let ONE_BD = BigDecimal.fromString("1");
+export let BI_18 = BigInt.fromI32(18);
 
 //subgraph dosen't support indexOf() now
-export function getAddressFirstIndex(addresses: string[], address: string): BigInt {
-    let index = BigInt.fromI32(-1);
-    let length = addresses.length;
+export function getAddressFirstIndex(
+  addresses: string[],
+  address: string
+): BigInt {
+  let index = BigInt.fromI32(-1);
+  let length = addresses.length;
 
-    for (let i = 0; i < length; i++) {
-
-        if (addresses[i] == address) {
-            index = BigInt.fromI32(i);
-            break
-        }
-
+  for (let i = 0; i < length; i++) {
+    if (addresses[i] == address) {
+      index = BigInt.fromI32(i);
+      break;
     }
-    return index
+  }
+  return index;
 }
 
 export function exponentToBigDecimal(decimals: BigInt): BigDecimal {
-    let bd = BigDecimal.fromString('1')
-    for (let i = ZERO_BI; i.lt(decimals as BigInt); i = i.plus(ONE_BI)) {
-        bd = bd.times(BigDecimal.fromString('10'))
-    }
-    return bd
+  let bd = BigDecimal.fromString("1");
+  for (let i = ZERO_BI; i.lt(decimals as BigInt); i = i.plus(ONE_BI)) {
+    bd = bd.times(BigDecimal.fromString("10"));
+  }
+  return bd;
 }
 
 export function bigDecimalExp18(): BigDecimal {
-    return BigDecimal.fromString('1000000000000000000')
+  return BigDecimal.fromString("1000000000000000000");
 }
 
 export function convertEthToDecimal(eth: BigInt): BigDecimal {
-    return eth.toBigDecimal().div(exponentToBigDecimal(18))
+  return eth.toBigDecimal().div(exponentToBigDecimal(BigInt.fromI32(18)));
 }
 
-export function convertTokenToDecimal(tokenAmount: BigInt, exchangeDecimals: BigInt): BigDecimal {
-    if (exchangeDecimals == ZERO_BI) {
-        return tokenAmount.toBigDecimal()
-    }
-    return tokenAmount.toBigDecimal().div(exponentToBigDecimal(exchangeDecimals))
+export function convertTokenToDecimal(
+  tokenAmount: BigInt,
+  exchangeDecimals: BigInt
+): BigDecimal {
+  if (exchangeDecimals == ZERO_BI) {
+    return tokenAmount.toBigDecimal();
+  }
+  return tokenAmount.toBigDecimal().div(exponentToBigDecimal(exchangeDecimals));
 }
 
 export function equalToZero(value: BigDecimal): boolean {
-    const formattedVal = parseFloat(value.toString())
-    const zero = parseFloat(ZERO_BD.toString())
-    if (zero == formattedVal) {
-        return true
-    }
-    return false
+  const formattedVal = parseFloat(value.toString());
+  const zero = parseFloat(ZERO_BD.toString());
+  if (zero == formattedVal) {
+    return true;
+  }
+  return false;
 }
 
 export function isNullEthValue(value: string): boolean {
-    return value == '0x0000000000000000000000000000000000000000000000000000000000000001'
+  return (
+    value ==
+    "0x0000000000000000000000000000000000000000000000000000000000000001"
+  );
 }
 
 export function fetchTokenSymbol(tokenAddress: Address): string {
-    // hard coded overrides
-    if (tokenAddress.toHexString() == '0xe0b7927c4af23765cb51314a0e0521a9645f0e2a') {
-        return 'DGD'
-    }
-    if (tokenAddress.toHexString() == '0x7fc66500c84a76ad7e9c93437bfc5ac33e2ddae9') {
-        return 'AAVE'
-    }
-    if (tokenAddress.toHexString() == '0xa0b86991c6218b36c1d19d4a2e9eb0ce3606eb48') {
-        return 'USDC'
-    }
+  // hard coded overrides
+  if (
+    tokenAddress.toHexString() == "0xe0b7927c4af23765cb51314a0e0521a9645f0e2a"
+  ) {
+    return "DGD";
+  }
+  if (
+    tokenAddress.toHexString() == "0x7fc66500c84a76ad7e9c93437bfc5ac33e2ddae9"
+  ) {
+    return "AAVE";
+  }
+  if (
+    tokenAddress.toHexString() == "0xa0b86991c6218b36c1d19d4a2e9eb0ce3606eb48"
+  ) {
+    return "USDC";
+  }
 
-    let contract = ERC20.bind(tokenAddress)
-    let contractSymbolBytes = ERC20SymbolBytes.bind(tokenAddress)
+  let contract = ERC20.bind(tokenAddress);
+  let contractSymbolBytes = ERC20SymbolBytes.bind(tokenAddress);
 
-    // try types string and bytes32 for symbol
-    let symbolValue = 'unknown'
-    let symbolResult = contract.try_symbol()
-    if (symbolResult.reverted) {
-        let symbolResultBytes = contractSymbolBytes.try_symbol()
-        if (!symbolResultBytes.reverted) {
-            // for broken pairs that have no symbol function exposed
-            if (!isNullEthValue(symbolResultBytes.value.toHexString())) {
-                symbolValue = symbolResultBytes.value.toString()
-            }
-        }
-    } else {
-        symbolValue = symbolResult.value
+  // try types string and bytes32 for symbol
+  let symbolValue = "unknown";
+  let symbolResult = contract.try_symbol();
+  if (symbolResult.reverted) {
+    let symbolResultBytes = contractSymbolBytes.try_symbol();
+    if (!symbolResultBytes.reverted) {
+      // for broken pairs that have no symbol function exposed
+      if (!isNullEthValue(symbolResultBytes.value.toHexString())) {
+        symbolValue = symbolResultBytes.value.toString();
+      }
     }
+  } else {
+    symbolValue = symbolResult.value;
+  }
 
-    return symbolValue
+  return symbolValue;
 }
 
 export function fetchTokenSymbolByCall(tokenAddress: Address): string {
-    // hard coded overrides
-    if (tokenAddress.toHexString() == '0xe0b7927c4af23765cb51314a0e0521a9645f0e2a') {
-        return 'DGD'
-    }
-    if (tokenAddress.toHexString() == '0x7fc66500c84a76ad7e9c93437bfc5ac33e2ddae9') {
-        return 'AAVE'
-    }
-    if (tokenAddress.toHexString() == '0xa0b86991c6218b36c1d19d4a2e9eb0ce3606eb48') {
-        return 'USDC'
-    }
+  // hard coded overrides
+  if (
+    tokenAddress.toHexString() == "0xe0b7927c4af23765cb51314a0e0521a9645f0e2a"
+  ) {
+    return "DGD";
+  }
+  if (
+    tokenAddress.toHexString() == "0x7fc66500c84a76ad7e9c93437bfc5ac33e2ddae9"
+  ) {
+    return "AAVE";
+  }
+  if (
+    tokenAddress.toHexString() == "0xa0b86991c6218b36c1d19d4a2e9eb0ce3606eb48"
+  ) {
+    return "USDC";
+  }
 
-    let contract = ERC20.bind(tokenAddress)
+  let contract = ERC20.bind(tokenAddress);
 
-    // try types string and bytes32 for symbol
-    let symbolValue = contract.symbol()
+  // try types string and bytes32 for symbol
+  let symbolValue = contract.symbol();
 
-    return symbolValue
+  return symbolValue;
 }
 
 export function fetchTokenName(tokenAddress: Address): string {
-    // hard coded overrides
-    if (tokenAddress.toHexString() == '0xe0b7927c4af23765cb51314a0e0521a9645f0e2a') {
-        return 'DGD'
-    }
-    if (tokenAddress.toHexString() == '0x7fc66500c84a76ad7e9c93437bfc5ac33e2ddae9') {
-        return 'Aave Token'
-    }
+  // hard coded overrides
+  if (
+    tokenAddress.toHexString() == "0xe0b7927c4af23765cb51314a0e0521a9645f0e2a"
+  ) {
+    return "DGD";
+  }
+  if (
+    tokenAddress.toHexString() == "0x7fc66500c84a76ad7e9c93437bfc5ac33e2ddae9"
+  ) {
+    return "Aave Token";
+  }
 
-    let contract = ERC20.bind(tokenAddress)
-    let contractNameBytes = ERC20NameBytes.bind(tokenAddress)
+  let contract = ERC20.bind(tokenAddress);
+  let contractNameBytes = ERC20NameBytes.bind(tokenAddress);
 
-    // try types string and bytes32 for name
-    let nameValue = 'unknown'
-    let nameResult = contract.try_name()
-    if (nameResult.reverted) {
-        let nameResultBytes = contractNameBytes.try_name()
-        if (!nameResultBytes.reverted) {
-            // for broken exchanges that have no name function exposed
-            if (!isNullEthValue(nameResultBytes.value.toHexString())) {
-                nameValue = nameResultBytes.value.toString()
-            }
-        }
-    } else {
-        nameValue = nameResult.value
+  // try types string and bytes32 for name
+  let nameValue = "unknown";
+  let nameResult = contract.try_name();
+  if (nameResult.reverted) {
+    let nameResultBytes = contractNameBytes.try_name();
+    if (!nameResultBytes.reverted) {
+      // for broken exchanges that have no name function exposed
+      if (!isNullEthValue(nameResultBytes.value.toHexString())) {
+        nameValue = nameResultBytes.value.toString();
+      }
     }
+  } else {
+    nameValue = nameResult.value;
+  }
 
-    return nameValue
+  return nameValue;
 }
 
 export function fetchTokenNameByCall(tokenAddress: Address): string {
-    // hard coded overrides
-    if (tokenAddress.toHexString() == '0xe0b7927c4af23765cb51314a0e0521a9645f0e2a') {
-        return 'DGD'
-    }
-    if (tokenAddress.toHexString() == '0x7fc66500c84a76ad7e9c93437bfc5ac33e2ddae9') {
-        return 'Aave Token'
-    }
+  // hard coded overrides
+  if (
+    tokenAddress.toHexString() == "0xe0b7927c4af23765cb51314a0e0521a9645f0e2a"
+  ) {
+    return "DGD";
+  }
+  if (
+    tokenAddress.toHexString() == "0x7fc66500c84a76ad7e9c93437bfc5ac33e2ddae9"
+  ) {
+    return "Aave Token";
+  }
 
-    let contract = ERC20.bind(tokenAddress)
+  let contract = ERC20.bind(tokenAddress);
 
-    // try types string and bytes32 for name
-    let nameValue = contract.name()
+  // try types string and bytes32 for name
+  let nameValue = contract.name();
 
-    return nameValue
+  return nameValue;
 }
 
 export function fetchTokenTotalSupply(tokenAddress: Address): BigInt {
-    if (tokenAddress.toHexString() == BASE_COIN) {
-        return BigInt.fromI32(0)
-    }
-    let contract = ERC20.bind(tokenAddress)
-    let totalSupplyResult = contract.try_totalSupply()
-    if (totalSupplyResult.reverted) {
-        return BigInt.fromI32(0)
-    }
-    return totalSupplyResult.value
+  if (tokenAddress.toHexString() == BASE_COIN) {
+    return BigInt.fromI32(0);
+  }
+  let contract = ERC20.bind(tokenAddress);
+  let totalSupplyResult = contract.try_totalSupply();
+  if (totalSupplyResult.reverted) {
+    return BigInt.fromI32(0);
+  }
+  return totalSupplyResult.value;
 }
 
 export function fetchTokenTotalSupplyByCall(tokenAddress: Address): BigInt {
-    if (tokenAddress.toHexString() == BASE_COIN) {
-        return BigInt.fromI32(0)
-    }
-    let contract = ERC20.bind(tokenAddress)
-    let totalSupplyResult = contract.totalSupply()
+  if (tokenAddress.toHexString() == BASE_COIN) {
+    return BigInt.fromI32(0);
+  }
+  let contract = ERC20.bind(tokenAddress);
+  let totalSupplyResult = contract.totalSupply();
 
-    return totalSupplyResult
+  return totalSupplyResult;
 }
 
 export function fetchTokenDecimals(tokenAddress: Address): BigInt {
-    // hardcode overrides
-    if (tokenAddress.toHexString() == '0x7fc66500c84a76ad7e9c93437bfc5ac33e2ddae9') {
-        return BigInt.fromI32(18)
-    }
-    if (tokenAddress.toHexString() == BASE_COIN) {
-        return BigInt.fromI32(18)
-    }
+  // hardcode overrides
+  if (
+    tokenAddress.toHexString() == "0x7fc66500c84a76ad7e9c93437bfc5ac33e2ddae9"
+  ) {
+    return BigInt.fromI32(18);
+  }
+  if (tokenAddress.toHexString() == BASE_COIN) {
+    return BigInt.fromI32(18);
+  }
 
-    let contract = ERC20.bind(tokenAddress)
-    // try types uint8 for decimals
-    let decimalValue = null
-    let decimalResult = contract.try_decimals()
-    if (!decimalResult.reverted) {
-        decimalValue = decimalResult.value
-    }
-    return BigInt.fromI32(decimalValue as i32)
+  let contract = ERC20.bind(tokenAddress);
+  // try types uint8 for decimals
+  let decimalValue = null;
+  let decimalResult = contract.try_decimals();
+  if (!decimalResult.reverted) {
+    decimalValue = decimalResult.value;
+  }
+  return BigInt.fromI32(decimalValue);
 }
 
 export function fetchTokenDecimalsByCall(tokenAddress: Address): BigInt {
-    // hardcode overrides
-    if (tokenAddress.toHexString() == '0x7fc66500c84a76ad7e9c93437bfc5ac33e2ddae9') {
-        return BigInt.fromI32(18)
-    }
-    if (tokenAddress.toHexString() == BASE_COIN) {
-        return BigInt.fromI32(18)
-    }
+  // hardcode overrides
+  if (
+    tokenAddress.toHexString() == "0x7fc66500c84a76ad7e9c93437bfc5ac33e2ddae9"
+  ) {
+    return BigInt.fromI32(18);
+  }
+  if (tokenAddress.toHexString() == BASE_COIN) {
+    return BigInt.fromI32(18);
+  }
 
-    let contract = ERC20.bind(tokenAddress)
-    let decimalResult = contract.decimals()
+  let contract = ERC20.bind(tokenAddress);
+  let decimalResult = contract.decimals();
 
-    return BigInt.fromI32(decimalResult as i32)
+  return BigInt.fromI32(decimalResult);
 }
 
-export function fetchTokenBalance(tokenAddress: Address, user: Address): BigInt {
-    if (tokenAddress.toHexString() == BASE_COIN) {
-        return BigInt.fromI32(0)
-    }
-    let contract = ERC20.bind(tokenAddress)
-    let balanceResult = contract.try_balanceOf(user);
-    if (balanceResult.reverted) {
-        return BigInt.fromI32(0)
-    }
+export function fetchTokenBalance(
+  tokenAddress: Address,
+  user: Address
+): BigInt {
+  if (tokenAddress.toHexString() == BASE_COIN) {
+    return BigInt.fromI32(0);
+  }
+  let contract = ERC20.bind(tokenAddress);
+  let balanceResult = contract.try_balanceOf(user);
+  if (balanceResult.reverted) {
+    return BigInt.fromI32(0);
+  }
 
-    return balanceResult.value;
+  return balanceResult.value;
 }
 
 export function createUser(address: Address, event: ethereum.Event): User {
-    let user = User.load(address.toHexString())
-    if (user === null) {
-        user = new User(address.toHexString())
-        user.timestamp = event.block.timestamp
-        user.updatedAt = event.block.timestamp;
-        user.save()
-    }
-    return user as User;
+  let user = User.load(address.toHexString());
+  if (user === null) {
+    user = new User(address.toHexString());
+    user.timestamp = event.block.timestamp;
+    user.updatedAt = event.block.timestamp;
+    user.save();
+  }
+  return user as User;
 }
 
 export function getDODOToken(): DodoToken {
-    let id = "DODO-Token"
-    let dodoToken = DodoToken.load(id);
-    if (dodoToken == null) {
-        dodoToken = new DodoToken(id);
-        dodoToken.tokens = ZERO_BI;
-        dodoToken.updatedAt = ZERO_BI;
-        dodoToken.save()
-    }
-    return dodoToken as DodoToken;
+  let id = "DODO-Token";
+  let dodoToken = DodoToken.load(id);
+  if (dodoToken == null) {
+    dodoToken = new DodoToken(id);
+    dodoToken.tokens = ZERO_BI;
+    dodoToken.updatedAt = ZERO_BI;
+    dodoToken.save();
+  }
+  return dodoToken as DodoToken;
 }
-
-
-
-
-
