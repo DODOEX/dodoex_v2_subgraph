896db88fd050bbdd64a47d97d6990c8a59907a49
pengyongjun <pengyongjun@vip.qq.com>
2023-11-22 17:13:57 +0800

    feat: support okb-x1-test chain

--- bin/config.js
+++ bin/config.js
@@ -11,6 +11,7 @@ const chains = [
   "okchain",
   "arbitrum",
   "polygon",
+  "optimism",
   "heco",
   "moonriver",
   "boba",
@@ -25,6 +26,7 @@ const chains = [
   "scroll",
   "manta",
   "mantle",
+  "tokb",
 ];
 const supportAlphaChains = ["arbitrum", "polygon", "mainnet"];
 const supportStudioChains = ["arbitrum", "polygon", "mainnet"];
