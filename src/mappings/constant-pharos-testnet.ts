a980a2bdb53b32d2d721e0b7656fefbb0eb9db59
yongjun <pengyongjun1991@gmail.com>
2025-06-18 22:38:28 +0800

    fix: redeploy pharos-testnet

--- src/mappings/constant-pharos-testnet.ts
+++ src/mappings/constant-pharos-testnet.ts
@@ -15,18 +15,18 @@ export const CHAIN_BASE_COIN_NAME = "ethereum";
 export const DIP3_TIMESTAMP = 1624442400;
 
 export const SMART_ROUTE_ADDRESSES: string[] = [
-  "0xDdfAddf910cED65F5856AbD37eff061914eebf3F",
+  "0x4b177AdEd3b8bD1D5D747F91B9E853513838Cd49",
 ];
-export const DVM_FACTORY_ADDRESS = "0x67aE5157007639c84B15540D5EC9b7a6C7449b4b";
-export const DPP_FACTORY_ADDRESS = "0x525BE0a25601379bF3f15B6Ae56Cc5CAf7BBd7d7";
+export const DVM_FACTORY_ADDRESS = "0xbA1d9EFA53Ac545779CBf483a192DacC06820fD2";
+export const DPP_FACTORY_ADDRESS = "0xeCF9631022e30e433a2a02E3bF8a7Ba2234F524b";
 export const CLASSIC_FACTORY_ADDRESS =
-  "0x460830473733c804bB550F913D65B5B88Af95eA9";
+  "0x8146e09183fb7ECae8339b31EE461Ef91aAd2E66";
 export const CROWDPOOLING_FACTORY_V2: string[] = [
-  "0x460830473733c804bB550F913D65B5B88Af95eA9",
+  "0x8146e09183fb7ECae8339b31EE461Ef91aAd2E66",
 ];
 
-export const DVM_FACTORY_DEPLOY_BLOCK = 9320888;
-export const DPP_FACTORY_DEPLOY_BLOCK = 9320888;
+export const DVM_FACTORY_DEPLOY_BLOCK = 9682639;
+export const DPP_FACTORY_DEPLOY_BLOCK = 9682639;
 
 export const ADDRESS_ZERO = "0x0000000000000000000000000000000000000000";
 export const BASE_COIN = "0xeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeee";
@@ -41,7 +41,7 @@ export const TRANSACTION_TYPE_CP_CLAIM = "CP_CLAIM";
 /**
  * usd pricing
  */
-export const WRAPPED_BASE_COIN = "0x844980602B159832C385e6dfD04f9A007C75e2Bf";
+export const WRAPPED_BASE_COIN = "0x3019b247381c850ab53dc0ee53bce7a07ea9155f";
 //pricing supported stable coins
 export const STABLE_ONE_ADDRESS = "0xb95ed7e958e196688984951f41ac2888f4b10ab9"; //usdt
 export const STABLE_TWO_ADDRESS = "0x46afe01d758a46d64c7d8e0791314d5db3e2e683"; //usdc
@@ -52,16 +52,16 @@ export const STABLE_COIN_PAIR_ONE =
 export const BASE_COIN_PAIR = "0x75f5d66a7bbb9330a9067c0833ec9b3198b71666"; //[WETH_USDC_PAIR]
 
 // AMM V2
-export const WETH_ADDRESS = "0x844980602B159832C385e6dfD04f9A007C75e2Bf";
-export const USDC_WETH_PAIR = "0x48249feeb47a8453023f702f15cf00206eebdf08"; // created
+export const WETH_ADDRESS = "0x3019b247381c850ab53dc0ee53bce7a07ea9155f";
+export const USDC_WETH_PAIR = "0x72df0bcd7276f2dFbAc900D1CE63c272C4BCcCED"; // created
 export const DAI_WETH_PAIR = "0x7d02a3e0180451b17e5d7f29ef78d06f8117106c"; // created block
-export const USDT_WETH_PAIR = "0x9c2dc7377717603eb92b2655c5f2e7997a4945bd"; // created block
+export const USDT_WETH_PAIR = "0xD4071393f8716661958F766DF660033b3d35fD29"; // created block
 // token where amounts should contribute to tracked volume and liquidity
 export let WHITELIST: string[] = [
   WETH_ADDRESS, // WETH
   "0x7d02a3e0180451b17e5d7f29ef78d06f8117106c", // DAI
-  "0x48249feeb47a8453023f702f15cf00206eebdf08", // USDC
-  "0x9c2dc7377717603eb92b2655c5f2e7997a4945bd", // USDT
+  "0x72df0bcd7276f2dFbAc900D1CE63c272C4BCcCED", // USDC
+  "0xD4071393f8716661958F766DF660033b3d35fD29", // USDT
 ];
 
 // minimum liquidity required to count towards tracked volume for pairs with small # of Lps
@@ -70,7 +70,7 @@ export let MINIMUM_USD_THRESHOLD_NEW_PAIRS = BigDecimal.fromString("400000");
 // minimum liquidity for price to get tracked
 export let MINIMUM_LIQUIDITY_THRESHOLD_ETH = BigDecimal.fromString("2");
 
-export const FACTORY_ADDRESS = "0xD3756Eb053Ff5B742Eb7FD7156DF6AF000cd7c38";
+export const FACTORY_ADDRESS = "0x7b29694E8d44c154E3a2c20A6A2D665D65264e6e";
 
 export let ZERO_BI = BigInt.fromI32(0);
 export let ONE_BI = BigInt.fromI32(1);
@@ -84,10 +84,10 @@ export let UNTRACKED_PAIRS: string[] = [
 ];
 
 // AMM V3
-export const factoryAddress = "0xfbbf8bfAa8dD792229e07F9EFE0935327414bC9f";
+export const factoryAddress = "0x711b476cbEb92803500Dea10CAeb35741d4c33f7";
 export const WHITELIST_TOKENS: string[] = WHITELIST;
 export const USDC_WETH_03_POOL = "0x4e0a1d4f6c2548550a983a0c446dd67abd00869d";
 export const stablecoinAddresses: string[] = [
-  "0x07d83526730c7438048d55a4fc0b850e2aab6f0b", // USDC
-  "0x9c2dc7377717603eb92b2655c5f2e7997a4945bd", // USDT
+  "0x72df0bcd7276f2dFbAc900D1CE63c272C4BCcCED", // USDC
+  "0xD4071393f8716661958F766DF660033b3d35fD29", // USDT
 ];
